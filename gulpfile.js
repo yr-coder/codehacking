e4aa899fa283eadec52cda9cc4bca9f8f8bc1919
yr-coder <rybalskiy@gmail.com>
2016-09-09 08:18:31 -0600

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,27 @@ require('laravel-elixir-vue');
  |
  */
 
+//noinspection BadExpressionStatementJS
 elixir(mix => {
     mix.sass('app.scss')
-       .webpack('app.js');
+    .styles([
+           'libs/blog-post.css',
+           'libs/font-awesome.css',
+           'libs/bootstrap.css',
+           'libs/metisMenu.css',
+           'libs/sb-admin-2.css'
+
+
+        ], './public/css/libs.css')
+
+    .scripts([
+
+        'libs/jquery.js',
+        'libs/bootstrap.js',
+        'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
+        'libs/scripts.js'
+
+    ], './public/js/libs.js')
+
 });
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('/admin', function (){
+
+    return view('admin.index');
+
+});
+
 
 Route::resource('admin/users', 'AdminUsersController');
